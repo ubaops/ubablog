76ee9c0acc9f30b100312322269dd8e174722a4a
uba <lsqtyihui@163.com>
2017-11-20 11:01:14 +0800

    change index.html

--- ubablog/settings.py
+++ ubablog/settings.py
@@ -75,20 +75,25 @@ WSGI_APPLICATION = 'ubablog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'uba_blog',#数据库名
+#        'USER':'uba',
+#        'PASSWORD':'uba',
+#        'HOST':'',
+#        'PORT':'',
+#        'OPTIONS':{
+#            'autocommit': True,
+#        }
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'uba_blog',#数据库名
-        'USER':'uba',
-        'PASSWORD':'uba',
-        'HOST':'',
-        'PORT':'',
-        'OPTIONS':{
-            'autocommit': True,
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
